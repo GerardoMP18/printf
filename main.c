4f455d816b861926c4f2ed4ceceebe7a322f5418
GerardoMP18 <gerardomarinparra18@gmail.com>
2022-03-14 16:26:16 -0500

    correction order percent

--- main.c
+++ main.c
@@ -14,12 +14,12 @@ int main(void)
 
 	len = _printf("Let's try to printf a simple sentence.\n");
 	len2 = printf("Let's try to printf a simple sentence.\n");
-	len = _printf("Percent:[%%]\n");
-	len2 = printf("Percent:[%%]\n");
 	_printf("Character:[%c]\n", 'H');
 	printf("Character:[%c]\n", 'H');
 	_printf("String:[%s]\n", "I am a string !");
 	printf("String:[%s]\n", "I am a string !");
+	len = _printf("Percent:[%%]\n");
+	len2 = printf("Percent:[%%]\n");
 
 
 	return (0);
