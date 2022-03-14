5058bb69d1a93fc724706b36374b86b5dee73931
GerardoMP18 <gerardomarinparra18@gmail.com>
2022-03-14 16:44:57 -0500

    Add include variable

--- main.c
+++ main.c
@@ -20,7 +20,7 @@ int main(void)
 	printf("String:[%s]\n", "I am a string !");
 	len = _printf("Percent:[%%]\n");
 	len2 = printf("Percent:[%%]\n");
-
-
+	_printf("Len:[%d]\n", len);
+	printf("Len:[%d]\n", len2);
 	return (0);
 }
