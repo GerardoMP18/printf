88eff4301efe11ce6b56a7da87dca9d91c2fc8e2
GerardoMP18 <gerardomarinparra18@gmail.com>
2022-03-14 15:19:15 -0500

    Adding requested operation to select the correct function

---
+++ get_printf.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * get_op_print - Function for produce output according to a format
+ * @format: List of parameters to traverse.
+ * Return: Gets the character according to the list of
+ * characters to be used.
+ */
+
+int (*get_op_print(const char *format))(va_list)
+{
+	int a;
+
+	op_t characteres[] = {
+		{"c", op_t_c},
+		{"s", op_t_s},
+		{"%", op_t_percent},
+		{NULL, NULL}
+	};
+
+	a = 0;
+
+	while (characteres[a].op)
+	{
+		if (*(characteres[a].op) == format[1])
+		{
+			return (characteres[a].f);
+		}
+		a++;
+	}
+
+	return (characteres[a].f);
+}
