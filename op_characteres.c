95fd18acabc263534ead2d6daf2190494756d15c
GerardoMP18 <gerardomarinparra18@gmail.com>
2022-03-14 15:14:25 -0500

    Adding functions of the different characters % c and s

---
+++ op_characteres.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ * op_t_c - Character input (c)
+ * @valist: parameter input
+ * Return: print requested character
+ */
+int op_t_c(va_list valist)
+{
+	char c;
+
+	c = va_arg(valist, int);
+	_putchar(c);
+	return (c);
+}
+/**
+ * op_t_s - Character input (s)
+ * @valist: parameter input
+ * Return: Print requested string
+ */
+int op_t_s(va_list valist)
+{
+	char *p;
+	int i = 0;
+	char resultado;
+
+	p = va_arg(valist, char *);
+
+	while (p[i] != '\0')
+	{
+		resultado += _putchar(p[i]);
+		i++;
+	}
+
+	return (resultado);
+}
+/**
+ * op_t_percent - Character input (%)
+ * @valist: paramater input
+ * Return: print %
+ */
+int op_t_percent(va_list valist);
+{
+	char i = valist;
+
+	i += _putchar('%');
+	return (i);
+}
