0dd0831332957511f770c277c301e813a10d9ac8
GerardoMP18 <gerardomarinparra18@gmail.com>
2022-03-14 15:23:14 -0500

    Adding a function that produces output with the correct format printf

---
+++ _printf.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdarg.h>
+
+/**
+ * _printf - Function for produces output according to a format
+ * @format: List of parameters to traverse
+ * Return: printf result
+ */
+
+int _printf(const char *format, ...)
+{
+	int i = 0, count = 0;
+	va_list valist;
+	int (*p)(va_list);
+
+	va_start(valist, format);
+	while (format[i] != '\0')
+	{
+		if (format[i] == '%')
+		{
+			p = get_op_print(format + i);
+
+			if (p != NULL)
+			{
+				count += p(valist);
+				i++;
+			}
+			else
+			{
+				count += _putchar(format[i]);
+			}
+		}
+		else
+		{
+			count += _putchar(format[i]);
+		}
+		i++;
+	}
+	va_end(valist);
+
+	return (count);
+}
