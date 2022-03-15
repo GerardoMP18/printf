a634582a07c53d0f99bceeb69fea557e89ff8914
GerardoMP18 <gerardomarinparra18@gmail.com>
2022-03-15 14:38:44 -0500

    Add new value

--- main.c
+++ main.c
@@ -9,18 +9,25 @@
  */
 int main(void)
 {
-	int len;
-	int len2;
+        int len;
+        int len2;
 
-	len = _printf("Let's try to printf a simple sentence.\n");
-	len2 = printf("Let's try to printf a simple sentence.\n");
-	_printf("Character:[%c]\n", 'H');
-	printf("Character:[%c]\n", 'H');
-	_printf("String:[%s]\n", "I am a string !");
-	printf("String:[%s]\n", "I am a string !");
-	len = _printf("Percent:[%%]\n");
-	len2 = printf("Percent:[%%]\n");
-	_printf("Len:[%d]\n", len);
-	printf("Len:[%d]\n", len2);
-	return (0);
+
+        len = _printf("Character:[%c]\n", 'H');
+        len2 = printf("Character:[%c]\n", 'H');
+        printf("Length:[%d, %i]\n", len, len);
+        printf("Length:[%d, %i]\n", len2, len2);
+        len = _printf("String:[%s]\n", "I am a string !");
+        len2 =printf("String:[%s]\n", "I am a string !");
+        printf("Len:[%d]\n", len);
+        printf("Len:[%d]\n", len2);
+        len = _printf("Percentttt:[%%]\n");
+        len2 = printf("Percentttt:[%%]\n");
+        printf("Len 1:[%d]\n", len);
+        printf("Len 2:[%d]\n", len2);
+        len = _printf("Percent:[%%]\n");
+        len2 = printf("Percent:[%%]\n");
+        printf("Len:[%d]\n", len);
+        printf("Len:[%d]\n", len2);
+        return (0);
 }
