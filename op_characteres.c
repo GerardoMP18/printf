d0d18472e5f2da246d83ae40d61d458c0ab1b959
GerardoMP18 <gerardomarinparra18@gmail.com>
2022-03-15 23:48:48 -0500

    Add validation for d and i

--- op_characteres.c
+++ op_characteres.c
@@ -16,7 +16,7 @@ int op_t_c(va_list valist)
 /**
  * op_t_s - Character input (s)
  * @valist: parameter input
- * Return: Print requested string
+ * Return: Print requested integer
  */
 int op_t_s(va_list valist)
 {
@@ -47,4 +47,44 @@ int op_t_percent(va_list valist)
 
 	_putchar('%');
 	return (1);
+}
+
+/**
+ * op_t_di - Character input (d and i)
+ * @valist: paramater input
+ * Return: print integer
+ */
+int op_t_di(va_list valist)
+{
+	int n;
+	int div;
+	int count;
+	unsigned int num;
+
+	div = 1;
+	count = 0;
+	n = va_arg(valist, int);
+
+	if (n < 0)
+	{
+		count += _putchar('-');
+		num = n * -1;
+	}
+	else
+	{
+		num = n;
+	}
+
+	while (num / div > 9)
+	{
+		div *= 10;
+	}
+
+	while (div != 0)
+	{
+		count = count + _putchar('0' + num / div);
+		num %= div;
+		div /= 10;
+	}
+	return (count);
 }
