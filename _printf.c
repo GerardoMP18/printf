c9e267741da6f8593a2eef29a47dfc10985828c4
GerardoMP18 <gerardomarinparra18@gmail.com>
2022-03-15 22:35:58 -0500

    Validation to valite null

--- _printf.c
+++ _printf.c
@@ -14,6 +14,12 @@ int _printf(const char *format, ...)
 	int (*p)(va_list);
 
 	va_start(valist, format);
+
+	if (format == NULL)
+		return (-1);
+	if (format[0] == '%' && format[1] == '\0')
+		return (-1);
+
 	while (format[i] != '\0')
 	{
 		if (format[i] == '%')
