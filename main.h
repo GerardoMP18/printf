ea993752d12eeb56982a41aa779eb68dd6257057
GerardoMP18 <gerardomarinparra18@gmail.com>
2022-03-27 01:00:15 -0500

    Adding function binary

--- main.h
+++ main.h
@@ -24,6 +24,7 @@ int op_t_c(va_list valist);
 int op_t_s(va_list valist);
 int op_t_percent(va_list valist);
 int op_t_di(va_list valist);
-
+int op_t_binary(va_list valist);
+int binary_recursive(unsigned int n);
 int (*get_op_print(const char *format))(va_list);
 #endif
